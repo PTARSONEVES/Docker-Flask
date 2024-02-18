af33c253c54c7fb830c7b58ed9c7fcb1970da6ec
Paulo de Tarso <ptarsoneves@gmail.com>
2024-02-18 19:16:27 -0300

    Poto de Galinhas 18_02_2024

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "mysql" ]
+if [ "$DATABASE"="mysql" ]
 then
+#    echo $DATABASE
     echo "Waiting for mysql..."
-
-#    while ! nc -z $SQL_HOST $SQL_PORT; do
+#    while ! nc -z $SQL_HOST $SQL_PORT; 
+#    while ! mysql --host='localhost' --user='root' --password='strolandia';
+#    while ! (mysqladmin ping);
+#    do
 #        sleep 0.1
-#   done
-
+#        sleep 3
+#        echo "aguardando o mysql..."
+#    done
     echo "Mysql started"
 fi
-
-#python manage.py create_db 
 exec "$@"
